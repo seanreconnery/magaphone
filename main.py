b3b15f6c6b55e35a4b07471993e3cdee79de6164
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2020-01-09 18:16:09 -0800

    re-organization of media assets
    /assets/audio
    /assets/images

--- main.py
+++ main.py
@@ -3,58 +3,48 @@ from kivy.app import App
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
-from kivy.uix.button import Button
 from kivy.core.audio import SoundLoader
 from kivy.uix.widget import Widget
 
-t1 = SoundLoader.load('mobilemagaphone_im-smart.mp3')
-t2 = SoundLoader.load('mobilemagaphone_china.mp3')
-t3 = SoundLoader.load('mobilemagaphone_congradulations.mp3')
-t4 = SoundLoader.load('russia-russia-russia.mp3')
-t5 = SoundLoader.load('mobilemagaphone_fake_news.mp3')
-t6 = SoundLoader.load('400lb-hacker.mp3')
-t7 = SoundLoader.load('mobilemagaphone_you-are-fake_news.mp3')
-t8 = SoundLoader.load('mobilemagaphone_dont-be-rude.mp3')
-t9 = SoundLoader.load('mobilemagaphone_shes-smart.mp3')
-t10 = SoundLoader.load('yuuge.mp3')
+t8 = SoundLoader.load('assets/mobilemagaphone_dont-be-rude.wav')
+t2 = SoundLoader.load('assets/mobilemagaphone_china.wav')
+t4 = SoundLoader.load('assets/russia-russia-russia.wav')
+t3 = SoundLoader.load('assets/mobilemagaphone_congradulations.wav')
+t5 = SoundLoader.load('assets/mobilemagaphone_fake_news.wav')
+t10 = SoundLoader.load('assets/yuuge.wav')
+t7 = SoundLoader.load('assets/mobilemagaphone_you-are-fake_news.wav')
+t6 = SoundLoader.load('assets/400lb-hacker.wav')
+t9 = SoundLoader.load('assets/mobilemagaphone_shes-smart.wav')
+t1 = SoundLoader.load('assets/mobilemagaphone_im-smart.wav')
 
 
 class Ta(ButtonBehavior, Image):
     def on_press(self):
-        # dont be rude
         t8.play()
-
 
 class Tb(ButtonBehavior, Image):
     def on_press(self):
         t2.play()
 
-
 class Tc(ButtonBehavior, Image):
     def on_press(self):
         t4.play()
 
-
 class Td(ButtonBehavior, Image):
     def on_press(self):
-        # Congradulations
         t3.play()
-
 
 class Te(ButtonBehavior, Image):
     def on_press(self):
         t5.play()
 
-
 class Tf(ButtonBehavior, Image):
     def on_press(self):
         t10.play()
 
-
 class Tg(ButtonBehavior, Image):
     def on_press(self):
         t7.play()
-
 
 class Th(ButtonBehavior, Image):
     def on_press(self):
@@ -72,12 +62,9 @@ class Tj(ButtonBehavior, Image):
 class MyGrid(Widget):
     pass
 
-
-class MyApp(App):
+class MAGAphone(App):
     def build(self):
         return MyGrid()
 
 if __name__ == '__main__':
-    MyApp().run()
-
-
+    MAGAphone().run()
